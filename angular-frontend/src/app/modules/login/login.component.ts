9c4fc4c826ce1a137d2823b48b9ab7f967d01908
delforojas <delfinrojasespina@gmail.com>
2025-10-09 07:58:37 +0200

    fix(frontend): footer clicable en login + mejora visual

--- angular-frontend/src/app/modules/login/login.component.ts
+++ angular-frontend/src/app/modules/login/login.component.ts
@@ -14,6 +14,7 @@ import { HttpErrorResponse } from '@angular/common/http';
  selector: 'app-login',
  standalone: true,
  templateUrl: './login.html',
+ styleUrls: ['./login.component.css'],
  imports:[FormsModule, ReactiveFormsModule, RouterModule ,FooterComponent],
 
 
--- angular-frontend/src/app/shared/components/footer.compomonent.ts
+++ angular-frontend/src/app/shared/components/footer.compomonent.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './footer.html',
+  styleUrl: 'footer.component.css'
 })
 export class FooterComponent   {
 
